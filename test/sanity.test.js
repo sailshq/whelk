417b1c6dbe57d58d76a36f0a3869b7687a23428d
Mike McNeil <mike@balderdash.co>
2015-07-19 04:31:14 -0500

    Stub environment var impl, as well as the setup for some tests

---
+++ test/sanity.test.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies
+ */
+
+var assert = require('assert');
+var Filesystem = require('machinepack-fs');
+
+
+
+
+describe('running a script', function (){
+
+  describe('that passes an empty machine def to `machine-as-script`', function (){
+
+    // before(function (done){
+    //   Filesystem.writeSync().execSync();
+    // });
+
+    // it('should not fail', function (){
+    //   // TODO
+    // });
+  });
+
+});
